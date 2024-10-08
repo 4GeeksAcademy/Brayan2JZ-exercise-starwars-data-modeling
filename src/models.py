94082e00322a75ab25806a898ee87d5d93787c17
Brayan2JZ <b.sredzinski92@gmail.com>
2024-10-08 19:29:04 +0000

    Commit

--- src/models.py
+++ src/models.py
@@ -7,26 +7,54 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False, unique=True)
+    password = Column(String(256), nullable=False)
+    favorites = relationship("Favorite", backref="user")
 
-class Address(Base):
-    __tablename__ = 'address'
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            "favorites": [favorite.serialize() for favorite in self.favorites]
+        }
+
+class Character(Base):
+    __tablename__ = 'character'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(45), nullable=False)
+    description = Column(String(450))
+    character_image = Column(String(512))
+    favorites = relationship("Favorite", backref="character")
 
-    def to_dict(self):
-        return {}
+class Planet(Base):
+    __tablename__ = 'planet'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(45), nullable=False)
+    description = Column(String(450))
+    planet_image = Column(String(512))
+    favorites = relationship("Favorite", backref="planet")
 
+class Favorite(Base):
+    __tablename__ = "favorite"
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("user.id", ondelete="CASCADE"), nullable=False)
+    character_id = Column(Integer, ForeignKey("character.id", ondelete="CASCADE"), nullable=True)
+    planet_id = Column(Integer, ForeignKey("planet.id", ondelete="CASCADE"), nullable=True)
+
+    def serialize(self):
+        return {
+            "type": "character" if self.character else "planet",
+            "id": self.character.id if self.character else self.planet.id,
+            "name": self.character.name if self.character else self.planet.name
+        }
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
